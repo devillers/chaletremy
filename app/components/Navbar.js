05322193c1c24cafd029eb1bc3779e6b1d0ed835
devillers <davidevillers@gmail.com>
2024-05-18 23:41:02 +0200

    navbar

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -77,7 +77,28 @@ const Navbar = () => {
                     <span
                       className={`${comforta.className} text-gray-800 hover:text-gray-600 px-3 py-2 rounded-md text-sm font-medium cursor-pointer`}
                     >
+                      Acces
+                    </span>
+                  </Link>
+                  <Link href="/contact">
+                    <span
+                      className={`${comforta.className} text-gray-800 hover:text-gray-600 px-3 py-2 rounded-md text-sm font-medium cursor-pointer`}
+                    >
+                      Séminaires
+                    </span>
+                  </Link>
+                  <Link href="/seminaire">
+                    <span
+                      className={`${comforta.className} text-gray-800 hover:text-gray-600 px-3 py-2 rounded-md text-sm font-medium cursor-pointer`}
+                    >
                       Contact
+                    </span>
+                  </Link>
+                  <Link href="/seminaire">
+                    <span
+                      className={`${comforta.className} text-gray-800 border-[1px] border-orange-700 hover:text-gray-600 px-3 py-2 rounded-md text-sm font-medium cursor-pointer`}
+                    >
+                      Réserver
                     </span>
                   </Link>
                 </div>
@@ -110,26 +131,55 @@ const Navbar = () => {
         </div>
         {isMenuOpen && (
           <div className="md:hidden">
-            <div className="ml-14 px-2 pt-2 pb-3 space-y-1 sm:px-3">
+            <div className="ml-14 px-2 pt-2 pb-3 space-y-1 sm:px-3 tracking-wide">
               <Link href="/">
                 <span
-                  className={`${comforta.className} text-gray-800 hover:text-gray-600 block px-3 py-2  cursor-pointer  text-[13px]`}
+                  className={`${comforta.className} text-gray-800 hover:text-gray-600 block px-3 py-2  cursor-pointer  text-[12px]`}
                 >
                   Accueil
                 </span>
               </Link>
               <Link href="/flat">
                 <span
-                  className={`${comforta.className} text-gray-800 hover:text-gray-600 block px-3 py-2  cursor-pointer  text-[13px]`}
+                  className={`${comforta.className} text-gray-800 hover:text-gray-600 block px-3 py-2  cursor-pointer  text-[12px]`}
                 >
                   L'appartement
                 </span>
               </Link>
+              <Link href="/seminaire">
+                <span
+                  className={`${comforta.className} text-gray-800 hover:text-gray-600 block px-3 py-2  cursor-pointer  text-[12px]`}
+                >
+                  Séminaires
+                </span>
+              </Link>
+
               <Link href="/contact">
                 <span
-                  className={`${comforta.className} text-gray-800 hover:text-gray-600 block px-3 py-2  cursor-pointer  text-[13px]`}
+                  className={`${comforta.className} text-gray-800 hover:text-gray-600 block px-3 py-2  cursor-pointer  text-[12px]`}
+                >
+                  Acces
+                </span>
+              </Link>
+              <Link href="/contact">
+                <span
+                  className={`${comforta.className} text-gray-800 hover:text-gray-600 block px-3 py-2  cursor-pointer  text-[12px]`}
+                >
+                  Activitées
+                </span>
+              </Link>
+              <Link href="/contact">
+                <span
+                  className={`${comforta.className} text-gray-800 hover:text-gray-600 block px-3 py-2  cursor-pointer  text-[12px]`}
                 >
                   Contact
+                </span>
+              </Link>
+              <Link href="/contact">
+                <span
+                  className={`${comforta.className} text-center rounded-md w-[100px] border-[2px] border-yellow-700 text-gray-800 hover:text-gray-600 block px-2 py-1 cursor-pointer  text-[12px]`}
+                >
+                  Réserver
                 </span>
               </Link>
             </div>
