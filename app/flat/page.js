0f0302dda70585844e44f8317cf91d3e4f5b925f
devillers <davidevillers@gmail.com>
2024-05-22 18:29:52 +0200

    shit

--- app/flat/page.js
+++ app/flat/page.js
@@ -1,18 +1,73 @@
 'use client';
 
+import { useState, useEffect, useRef } from 'react';
 import { comforta, lobster } from '../font';
-import VideoPlayer from '../components/VideoInsta';
 import Arrow from '../components/ArrowUp';
 import ArrowDown from '../components/ArrowDown';
 
 export default function Home() {
+  const [isVideoLoaded, setIsVideoLoaded] = useState(false);
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const handleVideoLoaded = () => {
+      setIsVideoLoaded(true);
+      if (videoRef.current) {
+        videoRef.current.play().catch((error) => {
+          console.error('Video playback failed:', error);
+        });
+      }
+    };
+
+    const videoElement = videoRef.current;
+    if (videoElement) {
+      videoElement.addEventListener('loadeddata', handleVideoLoaded);
+    }
+
+    return () => {
+      if (videoElement) {
+        videoElement.removeEventListener('loadeddata', handleVideoLoaded);
+      }
+    };
+  }, []);
+
   return (
     <>
       <section className="relative rounded-sm h-screen bg-cover bg-center flex flex-col justify-center items-center md:bg-chalet-app">
-        {/* Apply the md:hidden class directly to ensure the video is hidden on medium screens */}
-        <div className="w-full h-full md:hidden">
-          <VideoPlayer src="/videos/9575733-hd_1080_1920_30fps.mp4" />
+        {/* Placeholder image for mobile view */}
+        {!isVideoLoaded && (
+          <img
+            src="/images/background-flat.png" // Replace with your placeholder image path
+            alt="Placeholder"
+            className="absolute inset-0 w-full h-full object-cover md:hidden"
+          />
+        )}
+
+        {/* Video player for mobile view */}
+        <div
+          className={`absolute inset-0 w-full h-full ${
+            isVideoLoaded ? '' : 'hidden'
+          } md:hidden`}
+        >
+          <video
+            ref={videoRef}
+            src="/videos/9575733-hd_1080_1920_30fps.mp4"
+            className="w-full h-full object-cover"
+            autoPlay
+            muted
+            loop
+          ></video>
         </div>
+
+        {/* Static image for medium and larger screens */}
+        <div className="hidden md:block absolute inset-0 w-full h-full">
+          <img
+            src="/images/background-flat.png" // Replace with your large image path
+            alt="Chalet"
+            className="w-full h-full object-cover"
+          />
+        </div>
+
         <div className="absolute inset-0 bg-black opacity-20 bg-blend-difference"></div>
         <div className="relative flex flex-col items-center justify-center h-full w-full mx-auto">
           <div className="text-center mt-20">
--- app/seminaire/page.js
+++ app/seminaire/page.js
@@ -1,20 +1,78 @@
 'use client';
 
+import { useState, useEffect, useRef } from 'react';
 import { comforta, lobster } from '../font';
-import VideoPlayer from '../components/VideoInsta';
 import Arrow from '../components/ArrowUp';
 import ArrowDown from '../components/ArrowDown';
 
 export default function Home() {
+  const [isVideoLoaded, setIsVideoLoaded] = useState(false);
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const handleVideoLoaded = () => {
+      setIsVideoLoaded(true);
+      if (videoRef.current) {
+        videoRef.current.play().catch((error) => {
+          console.error('Video playback failed:', error);
+        });
+      }
+    };
+
+    const videoElement = videoRef.current;
+    if (videoElement) {
+      videoElement.addEventListener('loadeddata', handleVideoLoaded);
+    }
+
+    return () => {
+      if (videoElement) {
+        videoElement.removeEventListener('loadeddata', handleVideoLoaded);
+      }
+    };
+  }, []);
+
   return (
     <>
-      <section className="relative rounded-sm h-screen bg-cover bg-center flex flex-col justify-center items-center">
-        <VideoPlayer src="/videos/8716790-hd_1280_720_25fps.mp4" />
+      <section className="relative rounded-sm h-screen bg-cover bg-center flex flex-col justify-center items-center md:bg-chalet-app">
+        {/* Placeholder image for mobile view */}
+        {!isVideoLoaded && (
+          <img
+            src="/images/background-seminaire.png" // Replace with your placeholder image path
+            alt="Placeholder"
+            className="absolute inset-0 w-full h-full object-cover md:hidden"
+          />
+        )}
+
+        {/* Video player for mobile view */}
+        <div
+          className={`absolute inset-0 w-full h-full ${
+            isVideoLoaded ? '' : 'hidden'
+          } md:hidden`}
+        >
+          <video
+            ref={videoRef}
+            src="/videos/8716790-hd_1280_720_25fps.mp4"
+            className="w-full h-full object-cover"
+            autoPlay
+            muted
+            loop
+          ></video>
+        </div>
+
+        {/* Static image for medium and larger screens */}
+        <div className="hidden md:block absolute inset-0 w-full h-full">
+          <img
+            src="/images/background-seminaire.png" // Replace with your large image path
+            alt="Chalet"
+            className="w-full h-full object-cover"
+          />
+        </div>
+
         <div className="absolute inset-0 bg-black opacity-20 bg-blend-difference"></div>
         <div className="relative flex flex-col items-center justify-center h-full w-full mx-auto">
           <div className="text-center mt-20">
             <h2 className={`${lobster.className} text-[40px] mb-4 text-white`}>
-              séminaires
+              Séminaire
             </h2>
             <p
               className={`${comforta.className} text-[15px] mb-8 font-poppin text-white`}
@@ -28,46 +86,9 @@ export default function Home() {
 
       <section
         id="about"
-        className=" p-6 flex-col justify-center items-center leading-6 text-justify"
+        className="p-6 flex-col justify-center items-center leading-6 text-justify"
       >
-        <div>
-          <p className={`${comforta.className} text-[12px] mb-4 `}>
-            Le CHALET REMY peut également accueillir vos réunions
-            professionnelles et vos séminaires d’entreprise. Quoi de mieux que
-            de réunir son équipe en pleine nature dans un écrin élégant ? Le
-            calme ambiant et le panorama époustouflant sur le Massif du
-            Mont-Blanc sont parfaits pour apporter de la sérénité à vos
-            collaborateurs. L’atmosphère des lieux est idéale pour rendre vos
-            moments de team building efficaces et inoubliables. Le CHALET REMY
-            offre également toutes les conditions favorables aux membres de vos
-            COMEX, CODIR, pour se retirer du tumulte et prendre le recul
-            nécessaire pour acter des décisions stratégiques et pérennes. Notre
-            conciergerie se charge des services annexes à l’hébergement, tels
-            que collation, repas, transports, excursions, animations…
-          </p>
-          <p className={`${comforta.className} text-[12px] mb-4 `}>
-            {' '}
-            N’hésitez-pas à nous interroger pour un devis sur-mesure !
-          </p>
-
-          <h4 className={`${lobster.className} text-[16px] mb-4 `}>
-            capacité des lieux
-          </h4>
-          <ul className={`${comforta.className} text-[12px] mb-4 `}>
-            <li>Bureau : 8 personnes</li>
-            <li>Salles de projection : 8/10 personnes + 14/16 personnes</li>
-            <li>Salle de réunion au rez-de-chaussée : 20 personnes</li>
-            <li>Réunion en terrasse : 30 personnes</li>
-          </ul>
-          <h4 className={`${lobster.className} text-[16px] mb-4 `}>
-            équipements
-          </h4>
-          <ul className={`${comforta.className} text-[12px] mb-4 `}>
-            <li>video projecteur</li>
-            <li>Equipement mobile pour visio conférence</li>
-            <li>paperboard</li>
-          </ul>
-        </div>
+        flat
       </section>
 
       <Arrow />
