3bd98d5a81814c9c8bd14edc80696a565a952d2a
devillers <davidevillers@gmail.com>
2024-05-23 20:59:39 +0200

    ggg

---
+++ app/components/Accordion.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { Cabine } from '../font';
+
+const Accordion = ({ title, children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleAccordion = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="border-b border-gray-200">
+      <button
+        className="w-full text-left p-4 focus:outline-none flex justify-between items-center"
+        onClick={toggleAccordion}
+      >
+        <h3 className={`${Cabine.className} text-lg `}>{title}</h3>
+        <span className="text-2xl text-pink-600">{isOpen ? '-' : '+'}</span>
+      </button>
+      {isOpen && <div className="p-4">{children}</div>}
+    </div>
+  );
+};
+
+export default Accordion;
--- app/flat/page.js
+++ app/flat/page.js
@@ -1,7 +1,6 @@
 'use client';
 
 import { comforta, Cabine } from '../font';
-//import VideoPlayer from '../components/VideoBackground';
 import Arrow from '../components/ArrowUp';
 import ArrowDown from '../components/ArrowDown';
 import PhotoGallery from '../components/PhotoGallery';
@@ -10,18 +9,12 @@ import { IoPeople } from 'react-icons/io5';
 import { IoBedOutline } from 'react-icons/io5';
 import { GiBunkBeds } from 'react-icons/gi';
 import { TbBath } from 'react-icons/tb';
+import Accordion from '../components/Accordion'; // Importer le composant Accordion
 
 export default function Home() {
   return (
     <>
-      {/* <section className="relative rounded-sm h-screen bg-cover bg-center flex flex-col justify-center items-center md:bg-chalet-app"> */}
       <section className="relative rounded-sm h-screen bg-cover bg-center flex flex-col justify-center items-center bg-chalet-app">
-        {/* Video only visible on mobile */}
-        {/* <div className="block md:hidden absolute inset-0 w-full h-full">
-          <VideoPlayer src="/videos/flat-video.mp4" />
-        </div> */}
-
-        {/* Overlay for darkening the background */}
         <div className="absolute inset-0 bg-gradient-radial from-slate-900 via-transparent to-slate-800 opacity-90 "></div>
         <div
           className={`${Cabine.className} absolute bottom-2 rounded-sm shadow-sm left-2 text-white text-[16px] p-4 bg-slate-500`}
@@ -44,60 +37,101 @@ export default function Home() {
         </div>
       </section>
 
-      {/* Content */}
-
       <section
         className={`${Cabine.className} bg-[#fefdfd] text-slate-600 p-2 flex-col justify-center items-center leading-6 text-justify `}
       >
-        <div className="  mt-10 mb-10  max-w-[800px] mx-auto ">
-          <h3 className=" text-[25px] ">L'appartement</h3>
+        <div className="mt-10 mb-10 max-w-[800px] mx-auto ">
+          <h3 className="text-[25px]">L'appartement</h3>
           <h4 className="uppercase mt-1 ml-4 tracking-wide text-[12px]">
             Saint Gervais les bains - Le bettex
           </h4>
           <div className="flex flex-col md:flex-row justify-items-start mt-4">
             <a
-              className={`${Cabine.className}  mt-1 ml-4 tracking-wide uppercase text-[12px] flex items-center`}
+              className={`${Cabine.className} mt-1 ml-4 tracking-wide uppercase text-[12px] flex items-center`}
             >
               <IoPeople className="h-[16px] w-[16px] mr-2 text-pink-700 " />6
               personnes + 2
             </a>
             <a
-              className={`${Cabine.className}  mt-1 ml-4 tracking-wide uppercase text-[12px] flex items-center`}
+              className={`${Cabine.className} mt-1 ml-4 tracking-wide uppercase text-[12px] flex items-center`}
             >
               <TbMeterSquare className="h-[20px] w-[20px] mr-2 text-pink-700 " />
               70 m2
             </a>
-
             <a
-              className={`${Cabine.className}  mt-1 ml-4 tracking-wide uppercase text-[12px] flex items-center`}
+              className={`${Cabine.className} mt-1 ml-4 tracking-wide uppercase text-[12px] flex items-center`}
             >
               <IoBedOutline className="h-[16px] w-[16px] mr-2 text-pink-700 " />
               1 chambre + 1 canapé convertible
             </a>
             <a
-              className={`${Cabine.className}  mt-1 ml-4 tracking-wide uppercase text-[12px] flex items-center`}
+              className={`${Cabine.className} mt-1 ml-4 tracking-wide uppercase text-[12px] flex items-center`}
             >
               <GiBunkBeds className="h-[20px] w-[20px] mr-2 text-pink-700 " />1
               dortoirs
             </a>
             <a
-              className={`${Cabine.className}  mt-1 ml-4 tracking-wide uppercase text-[12px] flex items-center`}
+              className={`${Cabine.className} mt-1 ml-4 tracking-wide uppercase text-[12px] flex items-center`}
             >
               <TbBath className="h-[20px] w-[20px] mr-2 text-pink-700 " />1 sdb
             </a>
           </div>
-          <p
-            className={`${Cabine.className}  mt-7  tracking-wide p-2 md:p-0 text-[13px] leading-8 text-justify`}
-          >
-            Bienvenue dans cet appartement au sein d'un chalet historique de
-            Saint-Gervais. Idéalement situé entre Chamonix et Megève, au pied
-            des pistes et des chemins de randonnées, vous voici en pleine
-            nature, subjugués par la vue imprenable sur le Mont Blanc depuis l'
-            immense terrasse. A l'intérieur, la décoration soignée et la
-            rénovation intégrale offrent désormais tout le confort moderne pour
-            un repos bien mérité après une longue journée au grand air. Un cadre
-            parfait pour des vacances inoubliables !
-          </p>
+          <div className="">
+            <p
+              className={`${Cabine.className}  mt-7 tracking-wide p-4 text-[13px] leading-8 text-justify`}
+            >
+              Bienvenue dans cet appartement au sein d'un chalet historique de
+              Saint-Gervais. Idéalement situé entre Chamonix et Megève, au pied
+              des pistes et des chemins de randonnées, vous voici en pleine
+              nature, subjugués par la vue imprenable sur le Mont Blanc depuis
+              l' immense terrasse. A l'intérieur, la décoration soignée et la
+              rénovation intégrale offrent désormais tout le confort moderne
+              pour un repos bien mérité après une longue journée au grand air.
+              Un cadre parfait pour des vacances inoubliables !
+            </p>
+            <Accordion title="Le Logement">
+              <p
+                className={`${Cabine.className} tracking-wide p-2 text-[13px] leading-8 text-justify`}
+              >
+                L'appartement se compose de 2 chambres équipées d'une literie
+                neuve et confortable : La chambre principale offre intimité et
+                confort pour les couples ou les personnes en quête de
+                tranquillité. Le couchage peut s'organiser soit en un grand lit
+                de 180 ou en 2 lits de 90. Un charmant dortoir peut accueillir
+                jusqu'à 4 personnes, dans des lits superposés. Pour répondre à
+                tous les besoins, un canapé-lit de qualité est également
+                disponible, permettant d'accueillir 2 personnes supplémentaires
+                avec tout le confort nécessaire. La salle de bains est équipée
+                de toutes les commodités modernes pour votre plus grand confort.
+                La rénovation de qualité et la décoration soignée créent une
+                atmosphère accueillante et reposante après une journée au grand
+                air. De retour de vos aventures, vous apprécierez le cadre
+                paisible et confortable de l'appartement. L'espace de vie
+                spacieux et lumineux vous invite à vous détendre et à vous
+                ressourcer. Vous pourrez préparer de délicieux repas dans la
+                cuisine entièrement équipée et les savourer autour de la table à
+                manger conviviale. A l'extérieur, la terrasse privative vous
+                attend pour un déjeuner face au Mont Blanc ou encore pour un
+                moment de détente allongé/e sur une chaise-longue. L'emplacement
+                de cet appartement est véritablement privilégié. Situé au départ
+                des pistes et des chemins de randonnée du Bettex, vous aurez un
+                accès direct aux plaisirs des sports de montagne dès que vous
+                franchirez la porte. L'hiver, le retour se fait ski aux pieds ou
+                par la navette en quelques minutes et grâce aux
+                sèche-chaussures/gants, vous débutez votre journée de ski
+                suivante pieds et mains au chaud ! L'ancien hôtel Chalet Rémy
+                offre également un parking privé ainsi qu'une borne de recharge
+                pour véhicules électriques pour rendre votre expérience encore
+                plus agréable. Réservez dès maintenant cet appartement
+                exceptionnel au départ des pistes du Bettex, dans l'ancien hôtel
+                Chalet Rémy. Que vous soyez passionné de ski, à la recherche
+                d'une escapade en montagne ou simplement en quête de
+                tranquillité, cet appartement est l'endroit parfait pour vous
+                détendre, vous amuser et profiter de vacances inoubliables dans
+                les Alpes.
+              </p>
+            </Accordion>
+          </div>
         </div>
         <div id="about">
           <PhotoGallery />
