b36e8b24ff1ab8bc47031966613e896335b6ac97
devillers <davidevillers@gmail.com>
2024-06-02 18:29:31 +0100

    popup

--- app/components/Gallery.js
+++ app/components/Gallery.js
@@ -27,7 +27,7 @@ const Gallery = () => {
 
   return (
     <div className="container mx-auto p-4">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {images.map((src, index) => (
           <div
             key={index}
